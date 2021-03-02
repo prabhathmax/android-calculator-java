f56de9d420a11a0389be48d0b40fc113254c367d
prabhath wijesinghe <wdprabhath@gmail.com>
2021-03-02 15:01:35 +0530

    calcultor basics

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,12 +10,15 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDel, btnEqual, btnDivide, btnMultiply, btnPlus, btnPercentage, btnPoint;
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDel, btnEqual, btnDivide, btnMultiply, btnPlus, btnMinus, btnPercentage, btnPoint;
     EditText display;
+    float mValOne, mValTwo;
+    boolean minus, divide, plus, multiply, percentage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         btn0 = (Button) findViewById(R.id.btn0);
         btn1 = (Button) findViewById(R.id.btn1);
@@ -30,10 +34,99 @@ public class MainActivity extends AppCompatActivity {
         btnDivide = (Button) findViewById(R.id.btnDivide);
         btnMultiply = (Button) findViewById(R.id.btnMultiply);
         btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnMinus = (Button) findViewById(R.id.btnMinus);
         btnPercentage = (Button) findViewById(R.id.btnPercentage);
         btnPoint = (Button) findViewById(R.id.btnPoint);
         btnDel = (Button) findViewById(R.id.btnDel);
         display = (EditText) findViewById(R.id.display);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "0");
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "9");
+            }
+        });
+
+        btnPoint.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + ".");
+            }
+        });
 
         btnDel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +134,108 @@ public class MainActivity extends AppCompatActivity {
                 display.setText("");
             }
         });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (display == null) {
+                    display.setText("");
+                } else {
+                    mValOne = Float.parseFloat(display.getText() + "");
+                    plus = true;
+                    display.setText(null);
+                }
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (display == null) {
+                    display.setText("");
+                } else {
+                    mValOne = Float.parseFloat(display.getText() + "");
+                    minus = true;
+                    display.setText(null);
+                }
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (display == null) {
+                    display.setText("");
+                } else {
+                    mValOne = Float.parseFloat(display.getText() + "");
+                    divide = true;
+                    display.setText(null);
+                }
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (display == null) {
+                    display.setText("");
+                } else {
+                    mValOne = Float.parseFloat(display.getText() + "");
+                    multiply = true;
+                    display.setText(null);
+                }
+            }
+        });
+
+        btnPercentage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (display == null) {
+                    display.setText("");
+                } else {
+                    mValOne = Float.parseFloat(display.getText() + "");
+                    percentage = true;
+                    display.setText(null);
+                }
+            }
+        });
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValTwo = Float.parseFloat(display.getText() + "");
+
+                if (plus == true) {
+                    display.setText(mValOne + mValTwo + "");
+                    plus = false;
+                }
+
+                if (minus == true) {
+                    display.setText(mValOne - mValTwo + "");
+                    minus = false;
+                }
+
+                if (multiply == true) {
+                    display.setText(mValOne * mValTwo + "");
+                    multiply = false;
+                }
+
+                if (divide == true) {
+                    display.setText(mValOne / mValTwo + "");
+                    divide = false;
+                }
+
+                if (percentage == true) {
+                    display.setText((mValOne * mValTwo)/100 + "");
+                    percentage = false;
+                }
+            }
+        });
+
     }
 }
